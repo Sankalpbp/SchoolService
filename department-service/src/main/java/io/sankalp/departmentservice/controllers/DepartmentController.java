c1cfc21a6ae6a83154d33816c11f7908ffe19b5f
Sankalpbp <sankalpnitj@gmail.com>
2022-11-16 06:23:37 +0530

    Setting up the /get method for the department service.

--- department-service/src/main/java/io/sankalp/departmentservice/controllers/DepartmentController.java
+++ department-service/src/main/java/io/sankalp/departmentservice/controllers/DepartmentController.java
@@ -1,13 +1,26 @@
 package io.sankalp.departmentservice.controllers;
 
+import io.sankalp.departmentservice.dtos.DepartmentDTO;
+import io.sankalp.departmentservice.services.FetchDepartmentsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
 
+    @Autowired
+    private FetchDepartmentsService departmentsService;
+
     @GetMapping("/")
     public String welcome () {
         return "Hey! We are going to provide all the details about the departments in the school.";
+    }
+
+    @GetMapping ("/get")
+    public List<DepartmentDTO> getDepartments () {
+        return departmentsService.getDepartments();
     }
 }
--- department-service/src/main/java/io/sankalp/departmentservice/services/FetchDepartmentsService.java
+++ department-service/src/main/java/io/sankalp/departmentservice/services/FetchDepartmentsService.java
@@ -1,7 +1,15 @@
 package io.sankalp.departmentservice.services;
 
+import io.sankalp.departmentservice.dtos.DepartmentDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FetchDepartmentsService {
+
+    public List<DepartmentDTO> getDepartments () {
+        return new ArrayList<>();
+    }
 }
